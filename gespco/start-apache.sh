a0f9e3e6e70d995fb63cd6a02d3ee188fbd0d68f
CarlosFJi <carlosfierrojimenez@gmail.com>
2024-04-06 14:29:41 +0200

    apache-server

---
+++ gespco/start-apache.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Substitute environment variables in Apache configuration
+envsubst < /usr/local/apache2/conf/httpd.conf.template > /usr/local/apache2/conf/httpd.conf
+
+# For debugging: print the PORT variable and the resulting httpd.conf
+echo "PORT: $PORT"
+# cat /usr/local/apache2/conf/httpd.conf
+
+# Start Apache in the foreground
+exec httpd -DFOREGROUND
